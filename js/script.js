76491ca376cb59f04862506de0cd2d3ee038322e
Connor McIntire <occult.grimoire@gmail.com>
2025-04-06 02:54:26 -0400

    Add a success popup
    Added a success popup to the transport button and page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function showSuccessMessage(event) {
+    event.preventDefault(); 
+    document.getElementById('successModal').classList.remove('hidden'); 
+    document.getElementById('rideForm').reset(); 
+  }
+  
+  
+  function closeSuccessModal() {
+    document.getElementById('successModal').classList.add('hidden'); 
+  }
